807b530195636b4f344a58ea0db85bb0806ed74f
Alex <oleksandr.ovcharenko@dci-student.org>
2025-01-25 23:02:01 +0100

    Initial commit

---
+++ src/main/java/com/example/ImmoTracker/ImmoTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ImmoTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmoTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmoTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ImmoTracker/ImmoTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ImmoTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmoTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
